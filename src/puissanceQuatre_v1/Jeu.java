73e02029e2d428e35881077f86e6f3a6f003f80e
Quarren <pares.thibault@outlook.com>
2021-10-26 10:29:51 +0200

    ajout méthodes victoire

--- src/puissanceQuatre_v1/Jeu.java
+++ src/puissanceQuatre_v1/Jeu.java
@@ -30,6 +30,38 @@ public class Jeu {
 		}
 		return res;
 	}
+	
+	public boolean gagnerVerticalement(int idJoueur) {
+		// TODO
+		return false;
+	}
+	
+	public boolean gagnerHorizontalement(int idJoueur) {
+		// TODO
+		return false;
+	}
+	
+	// Nord Ouest
+	public boolean gagnerEnDiagonaleNO(int idJoueur) {
+		// TODO
+		return false;
+	}
+	
+	// Nord Est
+	public boolean gagnerEnDiagonaleNE(int idJoueur) {
+		// TODO
+		return false;
+	}
+	
+	public boolean ajouterPion(int idColonne, int idJoueur) {
+		// TODO
+		if (this.gagnerVerticalement(idJoueur) || this.gagnerHorizontalement(idJoueur) || this.gagnerEnDiagonaleNO(idJoueur) || 
+				this.gagnerEnDiagonaleNE(idJoueur)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		Jeu j = new Jeu();
@@ -37,6 +69,7 @@ public class Jeu {
 		j.colonnes[0].ajouterPion(1);
 		j.colonnes[1].ajouterPion(2);
 		j.colonnes[0].ajouterPion(1);
+		j.colonnes[0].ajouterPion(2);
 		System.out.println(j.toString());
 		//System.out.println("" + joueurs[0].getId() + joueurs[1].getId());
 		//System.out.println()
