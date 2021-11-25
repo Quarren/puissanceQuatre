41dfe2b4de7b08a1314c879fd5912b0d48b98d2d
parest <pares.thibault@outlook.com>
2021-11-25 10:32:01 +0100

    methodes diagonale OK - à tester cependant

--- src/puissanceQuatre_v3/JoueurAuto.java
+++ src/puissanceQuatre_v3/JoueurAuto.java
@@ -3,7 +3,7 @@ package puissanceQuatre_v3;
 public abstract class JoueurAuto extends Joueur {
 	
 	public int [] heuristics = {0, 2, 3, 1000};
-	public int colMilieu = 5;
+	public int prioMilieu = 5;
 	
 	public JoueurAuto(String couleur, Planche planche) {
 		super(couleur, planche);
@@ -23,18 +23,22 @@ public abstract class JoueurAuto extends Joueur {
 		//System.out.println("planche p2 après ajout");
 		//p2.printPlanche();
 		int idx = p2.dernierPieceLigne(col);
-		int nbpAlignes = 0;
-		//System.out.println("nbpAlignes : " + nbpAlignes);
+		int streak = 0;
+		//System.out.println("streak : " + streak);
 		for (int i = idx; i <= idx+3 && i <= 5; i++) {
 			//System.out.println("i = " + i);
 			if (p2.puissance4Planche[i][col].getCouleur() == this.getCouleur()) {
-				nbpAlignes++;
-				//System.out.println("nbpAlignes : " + nbpAlignes);
+				streak++;
+				//System.out.println("streak : " + streak);
 			} else {
 				break;
 			}
 		}
-		return nbpAlignes == 0 ? 0 : heuristics[nbpAlignes-1];
+		int score = col == 3 ? this.prioMilieu : 0;
+		if (streak != 0) {
+			score += heuristics[streak-1];
+		}
+		return score;
 	}
 	
 	// calcule le score horizontal quand on rajoute une pièce dans la colonne col
@@ -52,30 +56,29 @@ public abstract class JoueurAuto extends Joueur {
 		//System.out.println("coldepart: " + colDepart);
 		int colFin = col >= 3 ? 6 : col + 3;
 		//System.out.println("colfin : " + colFin);
-		int score = 0;
+		int score = col == 3 ? this.prioMilieu : 0;
 		while (colDepart + 3 <= colFin) {
-			int nbpAlignes = 0;
+			int streak = 0;
 			for (int i = 0; i < 4; i++) {
 				//System.out.println("itération n°" + i);
 				if (p2.puissance4Planche[idx][colDepart+i] != null) {
 					if (p2.puissance4Planche[idx][colDepart+i].getCouleur() == this.getCouleur()) {
-						nbpAlignes++;
-						//System.out.println(nbpAlignes);
+						streak++;
+						//System.out.println(streak);
 					} else {
 						break;
 					}
 				}
 			}
-			if (nbpAlignes != 0) {
-				System.out.println("nbpAlignes : " + nbpAlignes);
-				score += this.heuristics[nbpAlignes-1];
+			if (streak != 0) {
+				System.out.println("streak : " + streak);
+				score += this.heuristics[streak-1];
 			}
 			colDepart++;
 		}
 		return score;
 	}
 	
-	//TODO scoreDiagSOToNE
 	public int scoreDiagSOToNE(int col) {
 		System.out.println("appel méthode scoreDiagSOToNE");
 		Planche p2 = new Planche(this.planche);
@@ -116,8 +119,7 @@ public abstract class JoueurAuto extends Joueur {
 			return 0;
 		}
 		
-		// TODO : vérifier si col >= 3
-		// si oui colDepart = col - 3
+		// trouver coordonnées départ de la diagonale
 		int colDepart;
 		int lineDepart;
 		if (col <= 3 || idx >= 2) {
@@ -135,11 +137,10 @@ public abstract class JoueurAuto extends Joueur {
 		System.out.println("colDepart = " + colDepart);
 		System.out.println("lineDepart = " + lineDepart);
 
-		int score = 0;
+		int score = col == 3 ? this.prioMilieu : 0;
 		
 		int streak;
-		int j = 0;
-		int k = lineDepart;
+		// TODO vérifier si c'est bien <= ou < strict pour (colDepart + 3 <= 7 && lineDepart - 3 >= 0)
 		while (colDepart + 3 <= 7 && lineDepart - 3 >= 0 && colDepart <= col && lineDepart >= idx) {
 			streak = 0;
 			for (int i = 0; i < 4; i++) {
@@ -162,4 +163,84 @@ public abstract class JoueurAuto extends Joueur {
 	
 	
 	//TODO scoreDiagNOToSE
+	public int scoreDiagNOToSE(int col) {
+		System.out.println("appel méthode scoreDiagSOToNE");
+		Planche p2 = new Planche(this.planche);
+		// colonne pleine
+		if (p2.dernierPieceLigne(col) == 0) {
+			System.out.println("colonne " + col + "pleine.");
+			return -1;
+		}
+		p2.ajouterPiece(col, this.getCouleur());
+		int idx = p2.dernierPieceLigne(col);
+		System.out.println("idx = " + idx);
+		p2.printPlanche();
+		
+		// éliminer les configurations où on a pas 4 cases alignées en diagonale
+		if (idx == 0 && col == 4
+				|| idx == 0 && col == 5
+				|| idx == 0 && col == 6) {
+			return 0;
+		}
+		if (idx == 1 && col == 5
+				|| idx == 1 && col == 6) {
+			return 0;
+		}
+		if (idx == 2 && col == 6) {
+			return 0;
+		}
+		
+		if (idx == 5 && col == 0
+				|| idx == 5 && col == 1
+				|| idx == 5 && col == 2) {
+			return 0;
+		}
+		if (idx == 4 && col == 0
+				|| idx == 4 && col == 1) {
+			return 0;
+		}
+		if (idx == 3 && col == 0) {
+			return 0;
+		}
+		
+		// trouver coordonnées départ de la diagonale
+		int colDepart;
+		int lineDepart;
+		if (col <= 3 || idx <= 3) { // la diagonale commence à une des extrémités de la planche
+			if (col <= idx) {
+				colDepart = 0;
+				lineDepart = idx - col;
+			} else {
+				colDepart = col - idx;
+				lineDepart = 0;
+			}
+		} else { // la diagonale ne commence pas à une extrémité de la planche
+			colDepart = col - 3; lineDepart = idx - 3;
+		}
+
+		System.out.println("colDepart = " + colDepart);
+		System.out.println("lineDepart = " + lineDepart);
+
+		int score = col == 3 ? this.prioMilieu : 0;
+		
+		int streak;
+		while (colDepart + 3 < 7 && lineDepart + 3 < 6 && colDepart <= col && lineDepart <= idx) {
+			streak = 0;
+			for (int i = 0; i < 4; i++) {
+				if (p2.puissance4Planche[lineDepart+i][colDepart+i] != null) {
+					if (p2.puissance4Planche[lineDepart+i][colDepart+i].getCouleur() == this.getCouleur()) {
+						streak++;
+						//System.out.println(streak);
+					} else {
+						break;
+					}
+				}
+			}
+			if (streak != 0) {
+				score += this.heuristics[streak-1];
+			}
+			colDepart++; lineDepart++;
+		}
+		return score;
+	}
 }
--- src/puissanceQuatre_v3/Main.java
+++ src/puissanceQuatre_v3/Main.java
@@ -10,15 +10,22 @@ public class Main {
 		Joueur j1 = new JoueurHumain("O", p);
 		Jeu jeu = new Jeu(j1, j2, p);
 		//jeu.commencerJeu();
-		p.ajouterPiece(0, "X");
+		//p.ajouterPiece(0, "X");
+		/*
 		p.ajouterPiece(1, "X");p.ajouterPiece(1, "X");
-		p.ajouterPiece(2, "X");p.ajouterPiece(2, "X");
+		p.ajouterPiece(2, "X");p.ajouterPiece(2, "X");p.ajouterPiece(2,  "X");
 		p.ajouterPiece(3, "X");p.ajouterPiece(3, "X");p.ajouterPiece(3, "X");p.ajouterPiece(3, "X");
-		p.ajouterPiece(4, "X");p.ajouterPiece(4, "X");p.ajouterPiece(4, "X");p.ajouterPiece(4, "X");p.ajouterPiece(4, "X");
+		p.ajouterPiece(4, "X");p.ajouterPiece(4, "X");p.ajouterPiece(4, "X");p.ajouterPiece(4, "X");//p.ajouterPiece(4, "X");
 		p.ajouterPiece(5, "X");p.ajouterPiece(5, "X");p.ajouterPiece(5, "X");p.ajouterPiece(5, "X");p.ajouterPiece(5, "X");
+		*/
 		p.printPlanche();
 		
-		System.out.println("score diagonale : " + j2.scoreDiagSOToNE(5));
+		p.ajouterPiece(4, "X");//p.ajouterPiece(4, "X");
+		p.ajouterPiece(3, "X");p.ajouterPiece(3, "X");//p.ajouterPiece(3, "X");
+		p.ajouterPiece(2, "X");p.ajouterPiece(2, "X");p.ajouterPiece(2, "X");p.ajouterPiece(2, "X");
+		p.ajouterPiece(1, "X");p.ajouterPiece(1, "X");p.ajouterPiece(1, "X");p.ajouterPiece(1, "X");p.ajouterPiece(1, "X");
+		
+		System.out.println("score diagonale : " + j2.scoreDiagNOToSE(3));
 
 		//p.printPlanche();
 	}
